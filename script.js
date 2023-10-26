c2c71ed7de626c883baa00af1680cce326285b1e
AvaStrOficial <91764815+AvastrOficial@users.noreply.github.com>
2023-10-25 19:52:24 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+     window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-128573348-1');
+        gtag('config', 'UA-128573348-2');
+ function button() {
+            document.getElementById('firstline').style.color = '#202020';
+        }
